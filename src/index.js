33a3c4bfb504c1fb69e9b3f4e49452eab40cd338
Hugarty Pedro <hp-ca@hotmail.com>
2019-08-20 18:33:27 -0300

    Adicinando redirecionamento para tela de login quando não há token no sessionStorage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import FormLogin from './components/FormLogin';
-import {notFound} from './components/NotFound';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import { notFound } from './components/NotFound';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={App}/>
-            <Route path="/login" component={FormLogin}/>
-            <Route component={notFound}/>
-        </Switch>
-    </BrowserRouter>
-    , document.getElementById('root'));
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" render={() => sessionStorage.getItem('tokenFeedback') === null ?
+        <Redirect to='/login' /> : <App />} />
+      <Route path="/login" component={FormLogin} />
+      <Route component={notFound} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
