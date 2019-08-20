c6bc1776a8d996ded5116c929415dfdf9cb4fea8
Hugarty Pedro <hp-ca@hotmail.com>
2019-08-19 22:30:37 -0300

    Tela de login implementada

--- src/api/ApiFeedBack.jsx
+++ src/api/ApiFeedBack.jsx
@@ -1,15 +1,37 @@
+const baseURL = "http://localhost:8080/"
+const postMethod = 'post';
 
 
-export const testConectionWithAPI = () => {
-
-    fetch('http://localhost:8080/me/api/test')
-    .then(function(response) {
-      return response.text();
+export const doLogin = loginBody => {
+  let header = new Headers();
+  header.append("Content-Type","application/json");
+    fetch(`${baseURL}auth`,
+    {
+      method:postMethod,
+      headers: header,
+      body: JSON.stringify(loginBody)
+    }).then(function(response) {
+      return response.json();
     })
-    .then(function(text) {
-      console.log(text);
+    .then(function(json) {
+      console.log(json);
     })
     .catch(function(error) {
-        console.log(error)
+      console.log(error)
     });
-}
+  }
+
+  
+  export const testConectionWithAPI = () => {
+  
+      fetch(`${baseURL}me/api/test`)
+      .then(function(response) {
+        return response.text();
+      })
+      .then(function(text) {
+        console.log(text);
+      })
+      .catch(function(error) {
+          console.log(error)
+      });
+  }
--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import { testConectionWithAPI } from '../api/ApiFeedBack';
+import { doLogin } from '../api/ApiFeedBack';
 
 
 export class FormLogin extends Component {
 
     constructor(){
         super();
-        this.state = {email: '', senha:''}
+        this.state = {email:'', senha:''}
     }
 
     sendLoginData = () =>{
-        testConectionWithAPI();
+        doLogin(this.state);
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.name]:event.target.value});
     }
 
     render() {
         return (
             <div>
+                <label htmlFor="email">
+                    email
+                    <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/>
+                </label>
+                <label htmlFor="senha">
+                    senha
+                    <input type="password" name="senha" value={this.state.senha}  onChange={this.handleChange}/>
+                </label>
                 <button onClick={this.sendLoginData}>enviar</button>
             </div>
         );
